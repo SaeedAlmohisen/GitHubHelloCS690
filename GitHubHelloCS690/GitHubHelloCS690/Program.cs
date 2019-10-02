a98ab6c0fe3b284e447783058a5321bf3e7c2a8d
SaeedAlmohisen <eng.salqarni92@gmail.com>
2019-10-01 23:53:05 -0400

    GitHubHelloCS690

---
+++ GitHubHelloCS690/GitHubHelloCS690/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHelloCS690
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello CS690");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
